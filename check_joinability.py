3c8c263fe229275648f666b45b41dbbe62c3b854
Donato1992 <81573394+Donato1992@users.noreply.github.com>
2022-12-15 10:45:55 +0100

    Donato

--- check_joinability.py
+++ check_joinability.py
@@ -13,7 +13,8 @@ if os.path.exists("check_joinability.log"):
 	os.remove("check_joinability.log")
 logging.basicConfig(filename='check_joinability.log',level=logging.DEBUG)
 
-DEFAULT_DIR = "datasets/"
+/python_joinability_git/DataLake_BigData/check_joinability.py
+DEFAULT_DIR = "./DATALAKEdatasets/"
 DEFAULT_DIMENSIONS_DIR="dimensions/"
 DEFAULT_COD_DIR = "_cod_lists/"
 DEFAULT_ENTRY_FILE = DEFAULT_COD_DIR+"list_sources"
