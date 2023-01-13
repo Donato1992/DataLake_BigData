f8f6d6cffa0d3303532d88d3c77e90b80402dbc0
Donato1992 <81573394+Donato1992@users.noreply.github.com>
2023-01-13 12:42:08 +0100

    Donato

--- sparql_query.py
+++ sparql_query.py
@@ -1,9 +1,28 @@
-import rdflib 
+import rdflib
+import logging
+import os
+import owlrl
+
+#Debugging
+LOG_FILE='sparql.log'
+if os.path.exists(LOG_FILE):
+	os.remove(LOG_FILE)
+logging.basicConfig(filename=LOG_FILE,level=logging.DEBUG)
 
 GRAPH = rdflib.Graph()
 GRAPH.parse("knowladge_graph.ttl",format="turtle")
 
-q=GRAPH.query("""SELECT ?x ?z ?y WHERE{?x owl:sameAs ?y. ?y property:rollup ?z}LIMIT 200""")
+type_dimension="iso2"
+#y è il continent
+#x è la dimensione
+owlrl.DeductiveClosure(owlrl.CombinedClosure.RDFS_OWLRL_Semantics,datatype_axioms=True).expand(GRAPH)
+q=GRAPH.query("""SELECT ?x ?y WHERE{{SELECT DISTINCT ?x ?y WHERE{?x property:rollup ?y. ?x property:inLevel level:country} ORDER BY ASC(?X)} UNION {SELECT DISTINCT ?x ?y WHERE{?x property:rollup ?k. ?k property:rollup ?y.?x property:inLevel level:region}ORDER BY ASC(?X)}}""")
+#q=GRAPH.query("""SELECT ?x ?y  WHERE {?x ?z ?y.FILTER(?z=property:inLevel && ?x!=member:month && ?x!=member:year)}""")
 
+temp=[]
 for row in q:
-    print(f"{row.x},{row.z}")
+    print(f"{row.x},{row.y}")
+    logging.debug(f"{row.x},{row.y}")
+    temp.append(str(row.y).split("#")[1])
+tt=set(temp)
+logging.debug(tt)
